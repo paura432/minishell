9ba79f4c81aa094026c2fe66ea77a90dad9622a1
paura432 <pauramosimo@gmail.com>
2024-03-04 12:39:59 +0100

    arreglo de cd y echo

--- created_comands.c
+++ created_comands.c
@@ -7,7 +7,7 @@
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 11:10:00 by marvin            #+#    #+#             */
 /*   Updated: 2024/02/26 11:10:00 by marvin           ###   ########.fr       */
-/*                                                                            */
+/*  (mini->input > 8 && mini->input < 13) ||                                                                           */
 /* ************************************************************************** */
 
 #include"minishell.h"
@@ -15,24 +15,25 @@
 int created_comands(t_mini *mini, char **env)
 {
     int i;
+    int bol;
 
     i = 0;
-    while((mini->input > 8 && mini->input < 13) || mini->input == 32)
-        mini->input++;
-//    printf("%s\n", mini->input);
-    if(!ft_strncmp("echo", mini->input,  4))
-        i = echo_comand(mini->input + 4);
-    if(!ft_strncmp("cd", mini->input, 2))
-        i = cd_comand(mini->input + 2);
+    bol = 0;
+    while((mini->input[i] > 8 && mini->input[i] < 13) || mini->input[i] == 32)
+        i++;
+    if(!ft_strncmp("echo", mini->input + i,  4))
+        bol = echo_comand(mini->input + i + 4);
+    if(!ft_strncmp("cd", mini->input + i, 2))
+        bol = cd_comand(mini->input + i + 2);
     // else if(!ft_strncmp("pwd", mini->input, 3))
-    //     i = cd_comand(mini->input + 3);
+    //     bol = cd_comand(mini->input + 3);
     // else if(!ft_strncmp("export", mini->input, 6))
-    //     i = cd_comand(mini->input + 7);
+    //     bol = cd_comand(mini->input + 7);
     // else if(!ft_strncmp("unset", mini->input, 5))
-    //     i = cd_comand(mini->input + 6);
+    //     bol = cd_comand(mini->input + 6);
     // else if(!ft_strncmp("env", mini->input, 3))
-    //     i = cd_comand(mini->input + 3);
-    return(i);
+    //     bol = cd_comand(mini->input + 3);
+    return(bol);
 }
 
 int cd_comand(char *input)
@@ -41,14 +42,11 @@ int cd_comand(char *input)
 
     i = 0;
     //saltamos todos los espacios
-    printf("%s\n", input);
-    if(input[i] != 32 && input[i] != 0)
-        return(0);
+    //printf("%s\n", input);
     while((input[i] > 8 && input[i] < 13) || input[i] == 32)
         i++;
-//    if(input[i] == '.'  && input[i + 1] == '.')
-//        return(1);
-    printf("%s\n", input);
+    if(input[i] == 0)
+        return(1);
     if((chdir(input + i)))
         return(0);
     return(1);
